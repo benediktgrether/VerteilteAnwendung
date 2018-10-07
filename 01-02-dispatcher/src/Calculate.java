e4fd66defdaac8032cf11b7c2109e0301d1f1395
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-07 23:14:33 +0200

    Calculate delete Whitespace

--- 01-02-dispatcher/src/Calculate.java
+++ 01-02-dispatcher/src/Calculate.java
@@ -16,18 +16,4 @@ public class Calculate extends Thread
         int newValue = f.f(value);
         results.insert(newValue);
     }
-
-    // public int result(){
-    //     return arr[];
-    // }
-    // public static int result(int x)
-    // {
-    //     int arr[] = new int[x];
-    //     for (int i = 0; i < arr.length; i++)
-    //     {
-    //         arr[i] = 2*x;
-    //         System.out.println(arr[i]);
-    //     }
-    //     return arr[x-1];
-    // }
 }
