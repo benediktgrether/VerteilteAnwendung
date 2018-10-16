7fb86b07c817a781eeeb9c2b7bc109289fcd4e92
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-16 20:41:22 +0200

    delet typo

--- 01-01-eggclock/src/EggClock.java
+++ 01-01-eggclock/src/EggClock.java
@@ -1,7 +1,6 @@
 public class EggClock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		eggclock(4, "running");
 		eggclock(3, "running");
 		eggclock(2, "running");
