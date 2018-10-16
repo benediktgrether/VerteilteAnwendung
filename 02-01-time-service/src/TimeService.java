a32812692fd8865ed071e3efe1e0b4078d7b66fd
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-16 20:18:22 +0200

    Work on Time Service Bug

--- 02-01-time-service/src/TimeService.java
+++ 02-01-time-service/src/TimeService.java
@@ -3,54 +3,72 @@ import java.io.BufferedWriter;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
-import java.io.IOException;
+// import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TimeService
 {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        while(true)
-        {
-            ServerSocket serverSocket = new ServerSocket(7500);
-            Socket socket = serverSocket.accept();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            writer.write("time service");
-            writer.newLine();
-            writer.flush();
+        try{
             while(true)
             {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String inputTerminal  = reader.readLine();
-
-
-                if(inputTerminal == null)
-                {
-                    socket.close();
-                }
-                else
-                {
-                    switch(inputTerminal)
+                ServerSocket serverSocket = new ServerSocket(7500);
+                Socket socket = serverSocket.accept();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                writer.write("time service");
+                writer.newLine();
+                writer.flush();
+                try{
+                    while(true)
                     {
-                        case "date":
-                        writer.write(Clock.date());
-                        writer.newLine();
-                        writer.flush();
-                        break;
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String inputTerminal  = reader.readLine();
 
-                        case "time":
-                        writer.write(Clock.time());
-                        writer.newLine();
-                        writer.flush();
-                        break;
 
-                        default:
-                        writer.write("end");
-                        socket.close();
+                        if(inputTerminal == null)
+                        {
+                            reader.close(); 
+                            writer.close();
+                            socket.close();
+                        }
+                        else
+                        {
+                            switch(inputTerminal)
+                            {
+                                case "date":
+                                writer.write(Clock.date());
+                                writer.newLine();
+                                writer.flush();
+                                break;
+
+                                case "time":
+                                writer.write(Clock.time());
+                                writer.newLine();
+                                writer.flush();
+                                break;
+
+                                default:
+                                writer.write("end");
+                                writer.newLine();
+                                writer.flush();                           
+                                socket.close();
+                                serverSocket.close();
+                            }
+                        }    
+                    // serverSocket.close();
                     }
                 }
+                catch(Exception e)
+                {
+                    System.out.println("Error Inner Loop");
+                }
             }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error Outer Loop");
         }
     }
 }
--- 03-01-client-time-service/src/TimeServiceClient.java
+++ 03-01-client-time-service/src/TimeServiceClient.java
@@ -1,40 +1,3 @@
-// import java.io.BufferedReader;
-// import java.io.BufferedWriter;
-// import java.io.OutputStream;
-// import java.io.OutputStreamWriter;
-// import java.io.InputStreamReader;
-// import java.io.IOException;
-// import java.net.ServerSocket;
-// import java.net.Socket;
-
-
-// public class TimeServiceClient
-// {
-//     public static String dateFromServer(String ip)
-//     {
-//         Socket socket = new Socket(ip, 7500); 
-//         System.out.println("Test");
-//         // BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-//         // writer.write("time service");
-//         // writer.newLine();
-//         // writer.flush();
-//         return Clock.date();
-//     }
-//     public static String timeFromServer(String ip)
-//     {
-//         return Clock.time();
-//     }
-
-//     public static void main(String[] args) throws IOException
-//     {
-//         // IP wird mit IP - Adresse ersetzt
-//         System.out.println("Test");
-//         // dateFromServer("141.28.140.249");
-//         System.out.print(dateFromServer("141.28.140.249"));
-//     }
-// }
-
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -47,9 +10,9 @@ public class TimeServiceClient
 	
 	public static void main(String[] args) 
 	{
-        // String ip = "141.28.104.156";
-        dateFromServer("141.28.140.156");
-        // timeFromServer(ip);
+        String ip = "192.168.178.67";
+        dateFromServer(ip);
+        timeFromServer(ip);
 	}
 	
 	public static String dateFromServer(String ip)
@@ -57,7 +20,7 @@ public class TimeServiceClient
 		String inbox = null;
 		try
 		{
-			Socket socket = new Socket(ip, 75);
+			Socket socket = new Socket(ip, 7500);
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			inbox = reader.readLine();
@@ -66,8 +29,8 @@ public class TimeServiceClient
 			writer.newLine();
 			writer.flush();
 			inbox = reader.readLine();
-            System.out.print(inbox);
-            // socket.close();
+            System.out.println(inbox);
+            socket.close();
 		}
 		catch (IOException e) 
 		{
@@ -79,9 +42,10 @@ public class TimeServiceClient
 	public static String timeFromServer(String ip)
 	{
 		String inbox = null;
+		System.out.println("Test");
 		try
 		{
-			Socket socket = new Socket(ip, 75);
+			Socket socket = new Socket(ip, 7500);
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			inbox = reader.readLine();
@@ -90,8 +54,8 @@ public class TimeServiceClient
 			writer.newLine();
 			writer.flush();
 			inbox = reader.readLine();
-            System.out.print(inbox);
-            // socket.close();
+            System.out.println(inbox);
+            socket.close();
 		}
 		catch (IOException e) 
 		{
