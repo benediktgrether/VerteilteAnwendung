78b668f70c932a1f752bcfe99ebdc99899ebf852
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-16 20:45:25 +0200

    Add writer and reader closed

--- 02-01-time-service/src/TimeService.java
+++ 02-01-time-service/src/TimeService.java
@@ -57,8 +57,8 @@ public class TimeService
                                 serverSocket.close();
                             }
                         }    
-                    reader.close(); 
-                    writer.close();
+                    // reader.close(); 
+                    // writer.close();
                     serverSocket.close();
                     }
                 }
