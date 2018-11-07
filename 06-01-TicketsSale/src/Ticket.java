cf2a67b16caf593ad7b70fc0cc7215fa6668c812
Benedikt Grether <benedikt@pixelarbeit.de>
2018-11-07 14:46:40 +0100

    finished tickets app

--- 06-01-TicketsSale/src/Ticket.java
+++ 06-01-TicketsSale/src/Ticket.java
@@ -1,12 +1,13 @@
-
 public class Ticket {
 
 	int nummer;
+	String lastName;
 	TicketStatus status;
 	
-	public Ticket(int nummer) {
+	public Ticket(int nummer, String lastName) {
 		
 		this.nummer = nummer;
+		this.lastName = lastName;
 		this.status = TicketStatus.FREE;
 		
 	}
--- 06-01-TicketsSale/src/TicketsSale.java
+++ 06-01-TicketsSale/src/TicketsSale.java
@@ -1,4 +1,4 @@
-import java.util.Vector;
+//import java.util.Vector;
 import java.util.Scanner;
 
 
@@ -9,14 +9,14 @@ import java.util.Scanner;
 
 public class TicketsSale {
 
-	private Ticket ticket;
-	private Ticket []ticketarray = new Ticket[100];
+	private Ticket []ticketarray = new Ticket[10];
 	private static boolean runTime = true;
+	private boolean open = true;
 
 	public TicketsSale()
 	{
 		for (int i = 0; i < ticketarray.length; i++) {
-			ticketarray[i] = new Ticket(i + 1 );
+			ticketarray[i] = new Ticket(i + 1, null );
 		}
 	}
 	
@@ -27,15 +27,38 @@ public class TicketsSale {
 	
 	public void runTimeTicket(TicketsSale tickets)
 	{
-		while(runTime = true)
+		while(runTime == true)
 		{
 			//consoleInput();
-
-			switch(consoleInput())
+			System.out.println("Overview O, Sell s, Reservation r, Buyreservation br, Cancle c, DebugClear clear ");
+			switch(consoleInput().toLowerCase())
 			{
 				case "overview": case "o":
-					System.out.println("Test");
 					tickets.overviewTickets();
+					break;
+				
+				case "sell": case "s":
+					tickets.buyFreeTickets(consoleInput());
+						break;
+				
+				case "reservation": case "r":
+					tickets.reservTickets(consoleInput());
+					break;
+					
+				case "sellreservation": case "sr":
+					tickets.buyReservTickets(consoleInput());
+					break;
+					
+				case "cancel": case "c":
+					tickets.cancelTickets(consoleInput());
+					break;
+					
+				case "clear":
+					tickets.clearReservation();
+					break;
+					
+				case "quit": case "q":
+					runTime = false;
 					break;
 			}
 
@@ -45,13 +68,25 @@ public class TicketsSale {
 	
 	private String consoleInput()
 	{
-		System.out.println("Overview O,");
-		System.out.print("Your input >");
+		System.out.print("Your input > ");
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		return input.toLowerCase();
+		return input;
 	}
 
+	private int inputValue(String seatNumber) {
+		try {
+			int value = Integer.parseInt(seatNumber);
+			return value;
+		}
+		catch(Exception E)
+		{
+			System.out.println("Wrong Input");
+			System.out.println();
+		}
+		
+		return -1;
+	}
 
 	public void overviewTickets()
 	{
@@ -68,30 +103,141 @@ public class TicketsSale {
 		}		
 	}
 	
-	public void sellFreeTickets()
+	public TicketStatus buyFreeTickets(String seatNumber)
 	{
-		
+			int value = inputValue(seatNumber);
+			try {
+				if(ticketarray[value - 1].status.equals(TicketStatus.FREE) && open == true)
+				{
+					System.out.println("Your Seat is booked");
+					System.out.println();
+					return ticketarray[value - 1].status = TicketStatus.SOLD;
+				}
+				else if(open == false)
+				{
+					System.out.println("Reservation closed");
+				}else {
+					System.out.println("Seat not free");
+					System.out.println();
+				}
+			}
+			catch(Exception E)
+			{
+				System.out.println("Array out of index");
+				System.out.println();
+			}
+		return null;
 	}
 	
 	
-	public void reservationTickets()
+	public TicketStatus reservTickets(String seatNumber)
 	{
-		
+		int value = inputValue(seatNumber);
+		System.out.print("Your Name: ");
+		String lastName = consoleInput();
+		try 
+		{
+			if(ticketarray[value-1].status.equals(TicketStatus.FREE) && open == true)
+			{
+				System.out.println("Your Seat is reserved");
+				ticketarray[value - 1].status = TicketStatus.RESERVED;
+				ticketarray[value - 1].lastName = lastName;
+				return ticketarray[value - 1].status;
+			}
+			else if(ticketarray[value-1].status.equals(TicketStatus.SOLD))
+			{
+				System.out.println("Seat already sold");
+			}
+			else if(open == false)
+			{
+				System.out.println("Reservation closed");
+			}
+			else
+			{
+				System.out.println("Seat already reserved");
+			}
+		}
+		catch(Exception E)
+		{
+			System.out.println("Array out of index");
+			System.out.println();
+		}
+		return null;
 	}
 	
-	public void sellReservationTickets()
+	public TicketStatus buyReservTickets(String seatNumber)
 	{
-		
+		int value = inputValue(seatNumber);
+		System.out.print("Your Name: ");
+		String lastName = consoleInput();
+		try 
+		{
+			if((ticketarray[value-1].status.equals(TicketStatus.RESERVED) 
+					&& open == true) && (ticketarray[value-1].lastName.equals(lastName)))
+			{
+				System.out.println("You bought your Seat");
+				return ticketarray[value - 1].status = TicketStatus.SOLD;
+			}
+			else if(ticketarray[value-1].status.equals(TicketStatus.FREE))
+			{
+				System.out.println("Seat is Free");
+			}
+			else
+			{
+				System.out.println("Seat already sold");
+			}
+		}
+		catch(Exception E)
+		{
+			System.out.println("Array out of index");
+			System.out.println();
+		}
+		return null;
 	}
 	
-	public void cancellationTickets()
+	public TicketStatus cancelTickets(String seatNumber)
 	{
-		
+		int value = inputValue(seatNumber);
+		try 
+		{
+			if(ticketarray[value-1].status.equals(TicketStatus.SOLD) || ticketarray[value-1].status.equals(TicketStatus.RESERVED))
+			{
+				System.out.println("You canceld your Seat");
+				ticketarray[value - 1 ].lastName = null;
+				return ticketarray[value - 1].status = TicketStatus.FREE;
+			}
+			else
+			{
+				System.out.println("Seat is already free");
+			}
+		}
+		catch(Exception E)
+		{
+			System.out.println("Array out of index");
+			System.out.println();
+		}
+		return null;
 	}
 	
-	public void clearReservation()
+	public boolean clearReservation()
 	{
-		
+		for(int i = 0; i < ticketarray.length; i++)
+		{
+		try 
+			{
+				if(ticketarray[i].status.equals(TicketStatus.RESERVED))
+				{
+					//open = false;
+					ticketarray[i].status = TicketStatus.FREE;
+				}
+			}
+			catch(Exception E)
+			{
+				System.out.println("Array out of index");
+				System.out.println();
+			}
+		}
+		return open = false;
 	}
 	
 
